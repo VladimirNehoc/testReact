add928f720a36255364aae11f9b12c224239daa6
Vladimir Nehoc <vladimirnehoc@mail.ru>
2020-03-11 12:00:50 +0400

    create build

---
+++ build/precache-manifest.1d373c3885b471b17710916c46bf74b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b298665ecd86bb374cbfc37f4ca82d4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fc50b84a70e0a243d1a",
+    "url": "/static/css/main.4e9688c7.chunk.css"
+  },
+  {
+    "revision": "b4d02b4e1d8a418017da",
+    "url": "/static/js/2.d9283e87.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d9283e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc50b84a70e0a243d1a",
+    "url": "/static/js/main.c8786fb5.chunk.js"
+  },
+  {
+    "revision": "f3067559c9d10ec5b030",
+    "url": "/static/js/runtime-main.5513197a.js"
+  }
+]);
